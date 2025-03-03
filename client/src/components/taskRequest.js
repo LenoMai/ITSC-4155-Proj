3634f7387cd9f769f101cc9a82467505ef743048
jjewett1 <jjewett1@uncc.edu>
2025-03-02 20:01:34 -0500

    Added task request form view

--- client/src/components/taskRequest.js
+++ client/src/components/taskRequest.js
@@ -1,7 +1,25 @@
 import '../css/taskRequest.css';
 
 function TaskRequest(){
-
+    return(
+        <div className="form-box">
+                    <form>
+                        <div className="question-box">
+                            <label>Task Title: </label>
+                            <input type="text" id="title" placeholder="Title..." required/>
+                        </div>
+                        <div className="question-box">
+                            <label>Description: </label>
+                            <input type="text" id="description" placeholder="Description..." required/>
+                        </div>
+                        <div className="question-box">
+                            <label>Location: </label>
+                            <input type="text" id="location" placeholder="Location..." required/>
+                        </div>
+                        <button type="submit">Post Task</button>
+                    </form>
+                </div>
+    );
 }
 
 export default TaskRequest
